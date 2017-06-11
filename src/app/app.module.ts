30612bdeec529ec536d4627d2d0ff47ff725067c
Mahmood Khan <mkhan@mercycorps.org>
2017-06-10 18:22:13 -0700

    update the app module file to include the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FieldDefinitionComponent } from './field-definition/field-definition.component';
+import { FormDefinitionComponent } from './form-definition/form-definition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FieldDefinitionComponent,
+    FormDefinitionComponent,
   ],
   imports: [
     BrowserModule
